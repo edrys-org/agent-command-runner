ae1e5973506689a9233fb5cedce676caf4e476f1
edrysorg <edrys.org@pm.me>
2022-03-02 22:51:20 +1000

    Don't assume config

--- command-runner.js
+++ command-runner.js
@@ -1,10 +1,8 @@
 async function run_command(command, timeout = 30000, host = "http://localhost", port = 8585, options = {}) {
-    host = Edyrs.module.config.command_runner_host || host
-    port = Edyrs.module.config.command_runner_port || port
 
     const abortController = new AbortController();
     const abortTimeout = setTimeout(() => abortController.abort(), timeout);
-    
+
     const response = await fetch(`${host}:${port}/${encodeURIComponent(command)}`, {
         ...options,
         signal: abortController.signal
